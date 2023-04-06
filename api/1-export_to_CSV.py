b4aa852825ce0036840e4f6bee88a6a9606d2a69
soO <444deph12@gmail.com>
2023-04-06 09:17:40 -0700

    fixing pep8

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -11,8 +11,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = sys.argv[1]
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-    response_user_name = requests.get(f"https://jsonplaceholder.typicode.com/users?id={employee_id}")
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    response_user_name = requests.get(
+        f"https://jsonplaceholder.typicode.com/users?id={employee_id}")
 
     task_lists = response.json()
     name_scrapping = response_user_name.json()
@@ -23,5 +25,6 @@ if __name__ == "__main__":
     with open(file_name, mode='w') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for i, task in enumerate(task_lists, start=1):
-            writer.writerow([f"{employee_id}", f"{employee_name}", f"{task['completed']}", f"{task['title']}"])
+            writer.writerow([f"{employee_id}", f"{employee_name}",
+                             f"{task['completed']}", f"{task['title']}"])
     print(f"Task data for employee {employee_name} saved to {file_name}")
