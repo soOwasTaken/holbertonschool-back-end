92429e0d1a68e070e6ca7bba68ce122b29481262
soO <444deph12@gmail.com>
2023-04-06 07:40:04 -0700

    try to add if __name__ == __main__:

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     num_completed_tasks = len(completed_tasks)
 
     print(
-        f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+        f"Employee {employee_name} is done with tasks\
+            ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task}")
