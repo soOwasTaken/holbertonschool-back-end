ad9f57b7d84ad36a0d61b055ac061b05990ee498
soO <444deph12@gmail.com>
2023-04-07 03:17:05 -0700

    add of task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Json to output in python"""
+import json
+import requests
+
+if __name__ == "__main__":
+    # Get the list of all tasks
+    response_tasks = requests.get("https://jsonplaceholder.typicode.com/todos")
+    all_tasks = response_tasks.json()
+
+    # Get the list of all users
+    response_users = requests.get("https://jsonplaceholder.typicode.com/users")
+    all_users = response_users.json()
+
+    # Create a dictionary of tasks for each user
+    user_tasks = {}
+    for user in all_users:
+        user_id = str(user["id"])
+        username = user["name"]
+        user_tasks[user_id] = []
+        for task in all_tasks:
+            if str(task["userId"]) == user_id:
+                user_tasks[user_id].append({"username": username, "task": task["title"], "completed": task["completed"]})
+
+    # Export the data to a JSON file
+    filename = "todo_all_employees.json"
+    with open(filename, "w") as f:
+        json.dump(user_tasks, f)
+
+    print(f"The data has been exported to {filename}")
