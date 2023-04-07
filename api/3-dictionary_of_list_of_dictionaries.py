f07860c8c29b5a3ed8311c9b940c610e940798cd
soO <444deph12@gmail.com>
2023-04-07 03:23:34 -0700

    added all task from all users

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -17,13 +17,11 @@ if __name__ == "__main__":
     for user in all_users:
         user_id = str(user["id"])
         username = user["name"]
-        user_tasks[user_id] = []
+        user_tasks[user_id] = {"username": username, "tasks": []}
         for task in all_tasks:
             if str(task["userId"]) == user_id:
-                user_tasks[user_id].append(
-                    {"username": username,
-                     "task": task["title"],
-                     "completed": task["completed"]})
+                user_tasks[user_id]["tasks"].append(
+                    {"task": task["title"], "completed": task["completed"]})
 
     # Export the data to a JSON file
     filename = "todo_all_employees.json"
