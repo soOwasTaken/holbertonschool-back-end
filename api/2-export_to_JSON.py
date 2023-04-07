b6613b3ef4c00fb0e76e1591b92bc2499d9d27d6
soO <444deph12@gmail.com>
2023-04-07 03:12:56 -0700

    should get all task now

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
 """Json to output in python"""
-import json
 import requests
 import sys
+import json
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Need employee_id")
+        print("need employee_id")
         sys.exit(1)
 
     employee_id = sys.argv[1]
+    response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    response_user_name = requests.get(
+        f"https://jsonplaceholder.typicode.com/users?id={employee_id}")
 
-    # Get the list of tasks for the given employee ID
-    response_tasks = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-    tasks = response_tasks.json()
+    task_lists = response.json()
+    name_scrapping = response_user_name.json()
+    tasks = []
+    for task_list in task_lists:
+        task = {
+            "task": task_list["title"],
+            "completed": task_list["completed"],
+            "username": name_scrapping[0]['username']
+        }
+        tasks.append(task)
 
-    # Get the name of the employee
-    response_name = requests.get(f"https://jsonplaceholder.typicode.com/users?id={employee_id}")
-    employee_name = response_name.json()[0]['name']
-
-    # Create a list of tasks for the employee
-    task_list = []
-    for task in tasks:
-        task_list.append({"task": task["title"], "completed": task["completed"], "username": employee_name})
-
-    # Print the tasks
-    print(f"Tasks for employee {employee_name} ({len(task_list)} total):")
-    for task in task_list:
-        print(f"\t{task['task']} - {'Done' if task['completed'] else 'Not done'}")
-
-    # Export the data to a JSON file
-    filename = f"{employee_id}.json"
-    with open(filename, "w") as f:
-        json.dump({employee_id: task_list}, f)
-
-    print(f"The data has been exported to {filename}")
+    with open(f"{employee_id}.json", "w") as outfile:
+        json.dump({employee_id: tasks}, outfile)
