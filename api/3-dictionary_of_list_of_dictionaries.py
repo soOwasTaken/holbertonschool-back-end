fc38ebfe4180e9c49355e342ed728ab2fabb315d
soO <444deph12@gmail.com>
2023-04-07 03:45:31 -0700

    add of task 3, why it work locally but not here :(

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Json to ouput in python"""
+"""Gather data from an API"""
+
 import json
 import requests
 
-users_url = "https://jsonplaceholder.typicode.com/users?id="
-todos_url = "https://jsonplaceholder.typicode.com/todos"
+USERS_URL = "https://jsonplaceholder.typicode.com/users?id="
+TODOS_URL = "https://jsonplaceholder.typicode.com/todos"
 
 
 def gather_todo_data():
-    """ Gather data about employees TODOs """
+    """Gather data about employees TODOs"""
 
-    response = requests.get(todos_url).json()
+    response = requests.get(TODOS_URL).json()
     todos_by_user = {}
     for todo in response:
         if todo['userId'] not in todos_by_user:
@@ -26,6 +27,6 @@ def gather_todo_data():
 
 
 def get_username(user_id):
-    """ Fetch username for given user ID """
-    response = requests.get(users_url + str(user_id)).json()
+    """Fetch username for given user ID"""
+    response = requests.get(USERS_URL + str(user_id)).json()
     return response[0]['username']
